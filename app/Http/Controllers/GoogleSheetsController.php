82203250f95838573481e311a09d147e0309609e
crisanch <cristian.sanchez@triciclos.cl>
2021-11-08 18:07:57 -0300

    feat(googlesheet): add udpadet and append in sheet

--- app/Http/Controllers/GoogleSheetsController.php
+++ app/Http/Controllers/GoogleSheetsController.php
@@ -10,6 +10,19 @@ class GoogleSheetsController extends Controller
 {
     public function sheetOperation(Request $request)
     {
+        // (new GoogleSheetsServices())->writeSheet([
+        //     [
+        //         'cris sanchez',
+        //         'cris@gmail.com'
+        //     ]
+        // ]);
+        (new GoogleSheetsServices())->appendSheet([
+            [
+                'cris sanchez',
+                'cris@gmail.com'
+            ]
+        ]);
+
         $data = (new GoogleSheetsServices())->readSheet();
         dd($data);
         return response()->json($data);
--- app/Http/Services/GoogleSheetsServices.php
+++ app/Http/Services/GoogleSheetsServices.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Services;
 
-use Google\Client;
-use Google\Service\Sheets;
-use Google\Service\ValueRange;
+// use Google\Client;
+// use Google\Service\Sheets;
+// use Google\Service\ValueRange;
+
+use Google_Client;
+use Google_Service_Sheets;
+use Google_Service_Sheets_ValueRange;
+
 
 
 class GoogleSheetsServices
@@ -14,26 +19,61 @@ class GoogleSheetsServices
 
     public function __construct()
     {
-        $this->client = $this->getClient();
-        $this->service = new Sheets($this->client);
+        $this->client = new Google_Client();
+        $this->client->setAuthConfig(storage_path('credentials.json'));
+        $this->client->addScope("https://www.googleapis.com/auth/spreadsheets");
+        $this->googleSheetService = new Google_Service_Sheets($this->client);
+        $this->service = new Google_Service_Sheets($this->client);
         $this->documentId = '1FYMwlxvRexxT8dqLtHvMe1uEBhOmhQasR-cmYT5xCcs';
         $this->range = 'A:Z';
     }
 
-    public function getClient()
-    {
-        $client = new Client();
-        $client->setApplicationName('Google Sheets Demo');
-        $client->setRedirectUri('http://localhost/goolesheets');
-        $client->setScopes(sheets::SPREADSHEETS);
-        $client->setAuthConfig('credentials.json');
-        $client->setAccessType('offline');
 
-        return $client;
-    }
     public function readSheet()
     {
         $doc = $this->service->spreadsheets_values->get($this->documentId, $this->range);
         return $doc;
+    }
+
+    public function writeSheet($values)
+    {
+
+        $body = new Google_Service_Sheets_ValueRange([
+            'values' => $values
+        ]);
+
+        $params = [
+            'valueInputOption' => 'RAW'
+        ];
+        $result = $this->service->spreadsheets_values->update(
+            $this->documentId,
+            $this->range,
+            $body,
+            $params
+        );
+    }
+
+    public function appendSheet($values)
+    {
+
+        // $values = [
+        //     [
+        //         // Cell values ...
+        //     ],
+        //     // Additional rows ...
+        // ];
+        $body = new Google_Service_Sheets_ValueRange([
+            'values' => $values
+        ]);
+
+        $params = [
+            'valueInputOption' => 'USER_ENTERED'
+        ];
+        $result = $this->service->spreadsheets_values->append(
+            $this->documentId,
+            $this->range,
+            $body,
+            $params
+        );
     }
 }
